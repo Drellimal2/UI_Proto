216f2a1f0efacf8399be28322107de115b79bafa
Drellimal2 <dmiller2310@hotmail.com>
2015-12-01 15:02:12 -0500

    For Justen senpai

--- js/MainController.js
+++ js/MainController.js
@@ -1,32 +1,41 @@
 app.controller('MainCtrl', ['$scope', function ($scope) {
 	
-	
+	$scope.selected = 0;
 	$scope.tabs = [
 		{
 			name: 'Schedule',
-			hidden: false
+			hidden: false,
+			value: 0
 			
 		},
 		{
 			name: 'Courses',
-			hidden: false
+			hidden: false,
+			value: 1
 			
 		},
 		{
 			name: 'Assignments',
-			hidden: false
+			hidden: false,
+			value: 2
 			
 		},
 		{
 			name: 'Tests',
-			hidden: false
+			hidden: false,
+			value: 3
 			
 		},
 		{
 			name: 'GPA',
-			hidden: false
+			hidden: false,
+			value: 4
 			
 		}
 	]; 
+	
+	$scope.setsel= function(val){
+		$scope.selected = val;
+	};
 	
 }]);
